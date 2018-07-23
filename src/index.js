a11b77e6706f6582d3043273bf42169704e88797
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-07-23 19:39:33 +0700

    create store for store multiple reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const productsReducer = function(state=[], action){
+  return state;
+}
+
+const cartReducer = function(state=[], action){
+  return state;
+}
+
+const allReducers = {
+  products: productsReducer,
+  shoppingCart: cartReducer
+}
+
+const rootReducer = combineReducers(allReducers)
+
+let store = createStore(rootReducer);
