aec4f22f285b304b14d5c20ab8598858bba7a85d
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-07-27 14:03:55 +0700

    working in cartReducer, action and dispatch

--- src/index.js
+++ src/index.js
@@ -22,9 +22,27 @@ const productsReducer = function(state=initialState, action){
   return state;
 }
 
+const ADD_TO_CART = 'ADD_TO_CART';
 
-const cartReducer = function(state=[], action){
-  return state;
+const cartReducer = function(state=initialState, action){
+  switch (action.type) {
+    case ADD_TO_CART: {
+      return {
+        ...state,
+        cart: [...state.cart, action.payload]
+      }
+    }
+    default:
+      return state;
+  }
+}
+
+
+function addToCart(product, quantity, unitCost) {
+  return {
+    type: ADD_TO_CART,
+    payload: { product, quantity, unitCost }
+  }
 }
 
 const allReducers = {
@@ -37,3 +55,14 @@ const rootReducer = combineReducers(allReducers)
 let store = createStore(rootReducer);
 
 console.log("initial state: ", store.getState());
+
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+store.dispatch(addToCart('Coffee 500gm', 1, 250));
+store.dispatch(addToCart('Flour 1kg', 2, 110));
+store.dispatch(addToCart('Juice 2L', 1, 250));
+
+unsubscribe();
