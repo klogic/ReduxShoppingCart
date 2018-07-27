e301742fa3e8afb379dc089086734068ea8d5fcf
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-07-27 21:24:56 +0700

    split code into structure file

--- src/index.js
+++ src/index.js
@@ -1,58 +1,6 @@
-import { combineReducers } from 'redux';
-import { createStore } from "redux";
+import store from './store.js';
+import { addToCart }  from './actions/cart-actions';
 
-
-
-const initialState = {
-  cart: [
-    {
-      product: 'bread 700g',
-      quantity: 2,
-      unitCost: 90
-    },
-    {
-      product: 'milk 500ml',
-      quantity: 1,
-      unitCost: 47
-    }
-  ]
-}
-
-const productsReducer = function(state=initialState, action){
-  return state;
-}
-
-const ADD_TO_CART = 'ADD_TO_CART';
-
-const cartReducer = function(state=initialState, action){
-  switch (action.type) {
-    case ADD_TO_CART: {
-      return {
-        ...state,
-        cart: [...state.cart, action.payload]
-      }
-    }
-    default:
-      return state;
-  }
-}
-
-
-function addToCart(product, quantity, unitCost) {
-  return {
-    type: ADD_TO_CART,
-    payload: { product, quantity, unitCost }
-  }
-}
-
-const allReducers = {
-  products: productsReducer,
-  shoppingCart: cartReducer
-}
-
-const rootReducer = combineReducers(allReducers)
-
-let store = createStore(rootReducer);
 
 console.log("initial state: ", store.getState());
 
